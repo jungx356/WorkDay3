ff36ccd639b643f5ce69023fa9edf504f6bba4de
idsc3101 <idsc3101@gmail.com>
2014-09-22 17:15:23 -0500

    good

--- IfElse/IfElse/Form1.vb
+++ IfElse/IfElse/Form1.vb
@@ -11,8 +11,18 @@ Public Class Form1
         Dim st1 As String = txtFirstNum.Text
         Dim st2 As String = txtSecondNum.Text
 
+        If Not (IsNumeric(st1) And IsNumeric(st2)) Then
+            MessageBox.Show("please type the number")
+            Return
+        End If
+
         num1 = CDbl(st1)
         num2 = CDbl(st2)
+
+        If Not ((num1 >= 0) Or (num2 >= 0)) Then
+            MessageBox.Show("please type non-negative number")
+            Return
+        End If
 
         Dim result As String
 
@@ -23,8 +33,15 @@ Public Class Form1
         Else
             result = "They are equal"
         End If
-
         txtResult.Text = result
+
+
+
+
+
     End Sub
 
+    Private Sub txtFirstNum_TextChanged(sender As Object, e As EventArgs) Handles txtFirstNum.TextChanged
+
+    End Sub
 End Class
